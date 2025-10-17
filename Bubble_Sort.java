6d5e99aae151f2bd3bfa8376071f00ea275f8cc9
Pablo <90610087+PabloSemparo@users.noreply.github.com>
2025-10-17 11:16:01 +0300

    Create Bubble_Sort.java

---
+++ Bubble_Sort.java
@@ -0,0 +1,13 @@
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    double temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
