113262faf3744e52a795642f0f580604c67dbba9
Pablo <90610087+PabloSemparo@users.noreply.github.com>
2026-04-22 12:10:58 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+// Приветственное сообщение
+console.log("Hello! I'm your coding fun fact guide!");
+
+// Переменные бота
+let botName = "CodeBuddy";
+let botLocation = "JavaScript Universe";
+let favoriteLanguage = "JavaScript";
+
+// Информация о боте
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+// codingFact 1
+let codingFact = `Did you know? ${favoriteLanguage} is widely used for both frontend and backend development!`;
+console.log(codingFact);
+
+// codingFact 2 (перезапись)
+codingFact = `${favoriteLanguage} powers millions of websites around the world!`;
+console.log(codingFact);
+
+// codingFact 3 (ещё одна перезапись)
+codingFact = `One cool thing about ${favoriteLanguage} is its huge ecosystem of libraries and frameworks.`;
+console.log(codingFact);
+
+// Прощальное сообщение
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
