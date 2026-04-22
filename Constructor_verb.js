c655de8aa451a357b8747806c396055d3ef8d19e
Pablo <90610087+PabloSemparo@users.noreply.github.com>
2026-04-22 13:32:45 +0300

    Create Constructor_verb.js

---
+++ Constructor_verb.js
@@ -0,0 +1,25 @@
+// Объявление переменных
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "mysterious cave";
+let adjective2 = "fiery";
+let noun2 = "gold";
+
+// Первая история
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+// Новые значения переменных
+adjective = "tiny";
+noun = "mouse";
+verb = "excited";
+place = "big city";
+adjective2 = "pink";
+noun2 = "cheese";
+
+// Вторая история
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);
