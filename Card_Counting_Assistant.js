21d03e22cfec8d7ca0e1d0c21b94bc084470c7c5
Pablo <90610087+PabloSemparo@users.noreply.github.com>
2026-05-27 17:38:15 +0300

    Update Card_Counting_Assistant.js

--- Card_Counting_Assistant.js
+++ Card_Counting_Assistant.js
@@ -1,1 +1,21 @@
+let count = 0;
 
+function cardCounter(card) {
+  // увеличение счёта
+  if (card === 2 || card === 3 || card === 4 || card === 5 || card === 6) {
+    count++;
+  }
+  // без изменений
+  else if (card === 7 || card === 8 || card === 9) {
+    count += 0;
+  }
+  // уменьшение счёта
+  else {
+    count--;
+  }
+
+  // решение
+  const decision = count > 0 ? "Bet" : "Hold";
+
+  return count + " " + decision;
+}
