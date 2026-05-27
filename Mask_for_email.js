7bd90713014f7ff1a8ef0345e616384027f21549
Pablo <90610087+PabloSemparo@users.noreply.github.com>
2026-05-27 15:56:28 +0300

    Create Mask_for_email.js

---
+++ Mask_for_email.js
@@ -0,0 +1,13 @@
+function maskEmail(email) {
+  const atIndex = email.indexOf("@");
+
+  const username = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
+
+  const masked =
+    username[0] +
+    "*".repeat(username.length - 2) +
+    username[username.length - 1];
+
+  return masked + domain;
+}
